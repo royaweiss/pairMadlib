0cbb66df8ac3f1a0d7b48785f9b957b2ef6d8b70
royaweiss <royaweiss1@gmail.com>
2015-07-06 21:10:23 -0400

    finished MadLib

--- client/templates/madLib/madLib.js
+++ client/templates/madLib/madLib.js
@@ -9,6 +9,11 @@ Meteor.startup(function() {
 		var noun = $("#noun").val();
 
 		console.log("exclamation: " + exclamation);
+
+		var mad_lib = "'" + exclamation + "!' he said, as he  " + adverb + " jumped onto his " + noun + " and "+ verb + " into the sunset.";
+
+		$("#mad_lib").hide();
+		$("#mad_lib").text(mad_lib).fadeIn("slow").css("color", "#0000ff");
 	});	
 });
 
